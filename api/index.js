f5de4556a65102690bf91e3c7ba2eee6eac330da
moogie3 <jefrydwijaya3@gmail.com>
2025-01-19 18:06:14 +0700

    finally right

--- api/index.js
+++ api/index.js
@@ -59,13 +59,9 @@ app.use(function (req, res, next) {
   next(createError(404));
 });
 
-
-const express = require("express");
-
 app.get("/", (req, res) => res.send("Express on Vercel"));
 
 app.listen(3000, () => console.log("Server ready on port 3000."));
 
 module.exports = app;
 
-module.exports = app;
